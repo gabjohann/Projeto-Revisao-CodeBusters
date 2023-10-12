ab13bd64fdc653514f27d0e4221035684208a16f
Nathan da Cruz <nathanbdacruz@gmail.com>
2023-10-12 17:23:36 -0300

    add: class produto

---
+++ Produto.java
@@ -0,0 +1,9 @@
+public class Produto {
+    private String nomeProduto;
+    private double pesoProduto;
+
+    public Produto(String nome, double peso) {
+        this.nomeProduto = nome;
+        this.pesoProduto = peso;
+    }
+}
