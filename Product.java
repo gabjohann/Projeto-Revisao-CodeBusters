f265e23f5241804643f5bfb4869cc1208ee98511
Nathan da Cruz <nathanbdacruz@gmail.com>
2023-10-12 20:09:23 -0300

    refactor: changed to english file's name and code

---
+++ Product.java
@@ -0,0 +1,9 @@
+public class Product{
+    private String productName;
+    private double productWeight;
+
+    public Product(String name, double weight) {
+        this.productName = name;
+        this.productWeight = weight;
+    }
+}
--- Produto.java
+++
@@ -1,9 +0,0 @@
-public class Produto {
-    private String nomeProduto;
-    private double pesoProduto;
-
-    public Produto(String nome, double peso) {
-        this.nomeProduto = nome;
-        this.pesoProduto = peso;
-    }
-}
