06ccfb1714af909f361a8b77f0e1d3e47f679957
Clarke Brasil <135156383+Clarke2302@users.noreply.github.com>
2023-10-13 17:58:57 -0300

    Create viewRelatory.java
    adding the ViewReport class, which will later become a feature, and adding comments.

---
+++ viewRelatory.java
@@ -0,0 +1,12 @@
+//imports
+
+public class viewRelatory {
+    
+//Statistical Data
+
+//View report of registered transports: (total cost, cost per segment, average cost per km,
+//average cost per product, total cost per segment, total cost for each mode of transport,
+//the total number of vehicles moved and the total number of items transported);
+
+//view = system.out.println("");
+}
