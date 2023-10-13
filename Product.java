c1404fd156f8c4bde9d8e363a6064635173fca38
Nathan da Cruz <nathanbdacruz@gmail.com>
2023-10-13 15:40:56 -0300

    add: getters and setters

--- Product.java
+++ Product.java
@@ -1,9 +1,25 @@
-public class Product{
+public class Product {
     private String productName;
     private double productWeight;
 
     public Product(String name, double weight) {
         this.productName = name;
         this.productWeight = weight;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public double getProductWeight() {
+        return productWeight;
+    }
+
+    public void setProductWeight(double productWeight) {
+        this.productWeight = productWeight;
     }
 }
