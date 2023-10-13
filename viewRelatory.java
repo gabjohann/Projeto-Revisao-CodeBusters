068ff9dc8c011feee6b842e17d754ac5e6efc83e
Clarke Brasil <135156383+Clarke2302@users.noreply.github.com>
2023-10-13 18:14:41 -0300

    Update viewRelatory.java
    adding the main function to show all the transport information already registered and putting variables just so that no ERROR occurs, but these variables will be created in previous parts.

--- viewRelatory.java
+++ viewRelatory.java
@@ -22,4 +22,32 @@ public class viewRelatory {
     }
 
     
+    public static void exibirRelatorio(Transport transp){
+        double totalCost;
+        double costPSegment;
+        String typeTransp;
+        double averageCostPKm;
+        double averageCostPProduct;
+        double totalCostPSegment;
+        double totalCostFTransport;
+        int vehDesl;
+        String nameProd;
+        int quantProd;
+
+
+        System.out.println("\n"+"------------------------------------"+
+                            "\n"+"     Relatório dos Transportes     "+"\n"+
+                            "\n"+"Caminhão: "+typeTransp+
+                            "\n"+"Custo Total: "+totalCost+
+                            "\n"+"Custo p/ Trecho: "+costPSegment+
+                            "\n"+"Custo Médio p/KM: "+averageCostPKm+
+                            "\n"+"Custo Médio por Tipo de Produto: "+averageCostPProduct+
+                            "\n"+"Custo Total p/ Trecho: "+totalCostPSegment+
+                            "\n"+"Custo Total p/ cada Transporte: "+totalCostFTransport+
+                            "\n"+"Número Total de Veículos Deslocados: "+vehDesl+
+                            "\n"+"Total de Itens Transportados: "
+                            +exibirProdEnviados(transp)+
+                            "\n"+"------------------------------------"+"\n");
+    }
+
 }
