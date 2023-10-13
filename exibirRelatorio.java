a2bd63dd242f4be2d0ca4c923a277ddb836f1804
Clarke Brasil <135156383+Clarke2302@users.noreply.github.com>
2023-10-13 16:56:20 -0300

    Update exibirRelatorio.java
    criando função de exibir todos os produtos enviados em transportes para facilitar na hora de exibir relatório

--- exibirRelatorio.java
+++ exibirRelatorio.java
@@ -9,4 +9,14 @@ public class exibirRelatorio{
 //o número total de veículos deslocados e o total de itens transportados);
 
 //exibir = system.out.println("");
+
+    public static void exibirProdEnviados(Transporte transp){ //criada para facilitar a funcionalidade exibir ---> exibir todos os produtos enviados em transportes
+
+        String nomeProd;
+        int quantProd;
+        for(Amarelinha lista : transp){ //array ou lista :  transp
+        
+            System.out.println("Nome do Produto: "+nomeProd +" Quantidade: "+quantProd);
+        }
+    }
 }
