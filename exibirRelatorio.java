66134dfad92d2416a4f2d370c3e9872f68abdcf5
Clarke Brasil <135156383+Clarke2302@users.noreply.github.com>
2023-10-13 16:49:58 -0300

    Create exibirRelatorio.java
    adicionando classe exibirRelatorio que depois se tornará uma funcionalidade, e adicionando comentários.

---
+++ exibirRelatorio.java
@@ -0,0 +1,12 @@
+//imports
+
+public class exibirRelatorio{
+
+//Dados Estatísticos
+
+//exibir relatório dos transportes cadastrados: (custo total, custo por trecho, custo médio p/ km,
+//custo médio p/ produto, custo total p/ trecho, custo total para cada modalidade de transporte,
+//o número total de veículos deslocados e o total de itens transportados);
+
+//exibir = system.out.println("");
+}
