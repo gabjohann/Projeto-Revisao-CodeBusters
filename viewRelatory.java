bdeb7441d4b36b96b946567a568a559ce7dfa0ff
Clarke Brasil <135156383+Clarke2302@users.noreply.github.com>
2023-10-13 18:04:14 -0300

    Update viewRelatory.java
    Creating a function to display all products shipped in transports to make it easier to display reports

--- viewRelatory.java
+++ viewRelatory.java
@@ -9,4 +9,17 @@ public class viewRelatory {
 //the total number of vehicles moved and the total number of items transported);
 
 //view = system.out.println("");
+
+    public static void viewProdSent(Transport transp){ //created to ease the funcionality view ---> view all the products sent in transports
+
+        String nameProd;
+        int quantProd;
+
+        for(Yellow lista : transp){ //array or list :  transp
+        
+            System.out.println("Product Name: "+nameProd +" Quantity: "+quantProd);
+        }
+    }
+
+    
 }
