d5306c88916aa5b5f87608e5d81c04f21d58d4f5
Gabriel(Gideon)Martins <gabrielmartinspastoriza3266@gmail.com>
2023-10-16 16:06:02 -0300

    fix: Deleting files because codes were refactored

---
+++ src/com/org/br/github/TheBestTruck.java
@@ -0,0 +1,28 @@
+public class TheBestTruck {
+    private String classification;
+    private double pricePerKm;
+    private double maximumCapacity;
+
+    public TheBestTruck(String classification, double pricePerKm, double maximumCapacity) {
+        this.classification = classification;
+        this.pricePerKm = pricePerKm;
+        this.maximumCapacity = maximumCapacity;
+    }
+
+     //Os get são usados para obter os valores das variáveis de instância, não utilzei setters para não alterar os valores das variáveis.
+    public String getClassification() {
+        return classification;
+    }
+
+
+    public double getMaximumCapacity() {
+        return maximumCapacity;
+    }
+
+
+    //Calcula o custo por km pegando o preço por km * a distância fornecida
+    public double calculateCostPerKm(double distance) {
+        return pricePerKm * distance;
+    }
+
+}
--- src/com/org/gabrielmartins/.idea/sonarlint/securityhotspotstore/index.pb
+++
@@ -1,3 +0,0 @@
-
-9
-	main.java,1\9\19f3e5e6ff78fe29cd0f52c2307e766792d7ed9d
