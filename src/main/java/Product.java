141d4a68849edb4f235fecc2ff340c3216c898bd
Lucas Gabriel <gabif.johann@gmail.com>
2023-10-17 12:10:04 -0300

    add: gradle

---
+++ src/main/java/Product.java
@@ -0,0 +1,35 @@
+public class Product {
+    private String productName;
+    private double productWeight;
+    private double totalWeight;
+
+    public Product(String name, double weight) {
+        this.productName = name;
+        this.productWeight = weight;
+        this.totalWeight = weight;
+    }
+
+    public double getProductWeight() {
+        return productWeight;
+    }
+
+    public void setProductWeight(double productWeight) {
+        this.productWeight = productWeight;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public double getTotalWeight() {
+        return totalWeight;
+    }
+
+    public void setTotalWeight(double totalWeight) {
+        this.totalWeight = totalWeight;
+    }
+}
---
+++ src/main/java/TruckCostBenefitCalculator.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class TruckCostBenefitCalculator {
+    //Recebe os caminhões de TheBestTruck
+    public static TheBestTruck findMostCostEffectiveTruck(TheBestTruck[] trucks, double distance, double weight) {
+        //Retorna os caminhões e começa um filtro para encontrar um caminhão que atenda a relação capacidade x preço por km
+        return Arrays.stream(trucks)
+                //Funções lambda para otimizar o código.
+                .filter(truck -> truck.getMaximumCapacity() >= weight)
+                .min(Comparator.comparingDouble(truck -> truck.calculateCostPerKm(distance)))
+                .orElse(null);
+    }
+}
